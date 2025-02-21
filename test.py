281e96a797f26e1e80d11dbd3de926eefcf5554e
aluxf <alexfooladi@hotmail.com>
2025-02-21 14:01:50 +0100

    create Environment tests

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import unittest
-from robot import Direction
+from robot import Direction, Environment
 
 
 class TestDirection(unittest.TestCase):
@@ -15,6 +15,45 @@ class TestDirection(unittest.TestCase):
         self.assertEqual(Direction.rotate_right(Direction.SOUTH), Direction.WEST)
         self.assertEqual(Direction.rotate_right(Direction.WEST), Direction.NORTH)
 
+class TestEnvironment(unittest.TestCase):
+    def test_is_valid_position(self):
+
+        # For clarity
+        x_max = 4
+        y_max = 4
+        x_min = 0
+        y_min = 0
+
+        # Test with 5x5 environment
+        env = Environment(x_max+1, y_max+1)
+
+        # TODO: Test all valid positions?
+        self.assertTrue(env.is_valid_position(0, 0))
+        self.assertTrue(env.is_valid_position(4, 4))
+
+        # X > X_max and Y > y_max
+        self.assertFalse(env.is_valid_position(x_max+1, y_max+1))
+
+        # X < x_min and Y < y_min
+        self.assertFalse(env.is_valid_position(x_min-1, y_min-1))
+
+        # X < x_min and Y > y_max
+        self.assertFalse(env.is_valid_position(x_min-1, y_max+1))
+
+        # X > x_max and Y < y_min
+        self.assertFalse(env.is_valid_position(x_max+1, y_min-1))
+
+        # X < x_min
+        self.assertFalse(env.is_valid_position(x_min-1, 4))
+
+        # Y < y_min
+        self.assertFalse(env.is_valid_position(4, y_min-1))
+
+        # X > x_max
+        self.assertFalse(env.is_valid_position(x_max+1, 4))
+
+        # Y > y_max
+        self.assertFalse(env.is_valid_position(4, y_max+1))
 
 if __name__ == '__main__':
     unittest.main()
